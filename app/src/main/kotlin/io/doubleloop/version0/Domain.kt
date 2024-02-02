84faa2fed3a659d570bda0e072de6c4880c55b3a
Matteo Baglini <matteo.baglini@gmail.com>
2024-02-02 22:28:52 +0100

    feat: add app version0 TODOs

---
+++ app/src/main/kotlin/io/doubleloop/version0/Domain.kt
@@ -0,0 +1,21 @@
+package io.doubleloop.version0
+
+/*
+    ## V0 - Focus on domain types
+
+    Describe the domain types using:
+      - native types, typealias, type wrappers, product and sum types.
+ */
+
+// TODO 1: Those type alias are only placeholders,  use correct/better type definitions.
+//  Remember to be as strict as possible but also to keep it simple.
+
+typealias Rover = Nothing
+typealias Position = Nothing
+typealias Orientation = Nothing
+
+typealias Planet = Nothing
+typealias Size = Nothing
+typealias Obstacle = Nothing
+
+typealias Command = Nothing
