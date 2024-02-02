513af5828aeb2a3ed26382fd9ddc06f2df977b54
Matteo Baglini <matteo.baglini@gmail.com>
2024-02-02 15:56:23 +0100

    fix: loggin error for webapp

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,12 @@ dependencies {
     implementation(platform("org.http4k:http4k-bom:5.13.2.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-jetty")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation("io.strikt:strikt-core:0.34.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testImplementation("org.http4k:http4k-testing-approval:5.13.0.0")
-    testImplementation("org.http4k:http4k-testing-hamkrest:5.13.0.0")
     testImplementation("org.http4k:http4k-testing-strikt:5.13.0.0")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
